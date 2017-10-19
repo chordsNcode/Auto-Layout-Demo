380a122c8f38459571a3df3430c6f7046db42431
Matt Dias <mdias@zipcar.com>
2017-10-19 10:03:54 -0400

    Adding the constraints we animated using the storyboard's method

--- AutoLayoutExample/ViewController.swift
+++ AutoLayoutExample/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var imageWidthConstraint: NSLayoutConstraint!
+	@IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
+
+	@IBOutlet weak var image: UIImageView!
+
+	@IBAction func gearTapped(_ sender: Any) {
+		self.imageWidthConstraint.constant *= 2
+		self.imageHeightConstraint.constant *= 2
+
+		let animation = CABasicAnimation(keyPath: #keyPath(CALayer.cornerRadius))
+		animation.fromValue = image.layer.cornerRadius
+		animation.toValue = image.layer.cornerRadius * 2
+		animation.duration = 4
+		image.layer.add(animation, forKey: "whatevs")
+		self.image.layer.cornerRadius *= 2
+
+		UIView.animate(withDuration: 4) {
+			self.view.layoutIfNeeded()
+		}
+
+	}
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
